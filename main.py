79af7c2821e1cd17361ae524f21955de17e0a9a8
tylernow <tylernow@umich.edu>
2025-03-27 11:57:13 -0400

    test

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import requests
 
+response = requests.get("https://wizard-world-api.herokuapp.com/Wizards")
+if response.status_code == 200:
+    data = response.json()
+    for wizard in data[:5]:
+        print(wizard['firstName'], wizard['lastName'])
